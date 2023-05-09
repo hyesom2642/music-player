685e96ba3423a74ad2a671394010378754d3bd29
hyesom2642 <kimhyesom@naver.com>
2023-05-10 00:42:26 +0900

    DB 기본 설정

--- src/init.js
+++ src/init.js
@@ -3,9 +3,21 @@ import "dotenv/config";
 import "./db";
 import app from "./server";
 
-const PORT = process.env.PORT || 4000;
+import dotenv from 'dotenv';
+dotenv.config();
 
-const handleListening = () =>
-  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+const app = express();
+const basic_port = 4000;
 
-app.listen(PORT, handleListening);
+app.set('port', process.env.PORT || basic_port);
+app.get('/', (req, res) => {
+  res.send('hello, Express');
+});
+app.listen(app.get('port'), () => {
+  console.log(`${app.get('port')}에서 대기중입니다.`);
+})
+
+// const handleListening = () =>
+//   console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+
+// app.listen(PORT, handleListening);

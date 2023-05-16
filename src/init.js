930784369be5f1e9ce880f6282989dc3316742f3
hyesom2642 <kimhyesom@naver.com>
2023-05-16 21:08:35 +0900

    🚧 기초 node.js 설정

--- src/init.js
+++ src/init.js
@@ -1,23 +1,45 @@
-import "regenerator-runtime";
-import "dotenv/config";
-import "./db";
-import app from "./server";
-
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
-const basic_port = 4000;
+console.log('api :', process.env.YOUTUBE_API_KEY);
 
-app.set('port', process.env.PORT || basic_port);
-app.get('/', (req, res) => {
-  res.send('hello, Express');
+import express from 'express';
+// const express = require('express');
+const app = express();
+
+import path from 'path';
+const __dirname = path.resolve();
+
+app.listen(4000, function() {
+  console.log('ok');
 });
-app.listen(app.get('port'), () => {
-  console.log(`${app.get('port')}에서 대기중입니다.`);
+app.use(express.static('assets'));
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 })
 
-// const handleListening = () =>
-//   console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
 
-// app.listen(PORT, handleListening);
+// import "regenerator-runtime";
+// // import "dotenv/config";
+// import "./db";
+// // import app from "./server";
+
+// import dotenv from 'dotenv';
+// dotenv.config();
+// console.log('api :', process.env.YOUTUBE_API_KEY);
+
+// // const app = express();
+// const basic_port = 4000;
+
+// // app.set('port', process.env.PORT || basic_port);
+// app.get('/', (req, res) => {
+//   res.send('hello, Express');
+// });
+// app.listen(4000, () => {
+//   console.log(`${app.get('port')}에서 대기중입니다.`);
+// })
+
+// // const handleListening = () =>
+// //   console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+
+// // app.listen(PORT, handleListening);
